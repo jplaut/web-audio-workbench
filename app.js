e598666e13b3b532f5b191e13a7731454a0728b1
Jon Plaut <jp@tangerinesymphony.net>
2013-01-03 12:26:13 -0800

    fixing port issue

--- app.js
+++ app.js
@@ -8,5 +8,8 @@ app.get('/', function(req, res) {
   res.render('index');
 });
 
-app.listen(4567, 'localhost');
-console.log('Listening on port 4567');
+var port = process.env.PORT || 4567;
+
+app.listen(port, function() {
+  console.log('Listening on port 4567');
+});
